3fe996b5990e2193997b75ec6ae8dec50ecb2434
emile catteeuw <emioe.catteeuw3@gmail.com>
2021-11-22 17:55:36 +0100

    added class to forminput in order to style form the right way #3

--- src/view/pages/register.php
+++ src/view/pages/register.php
@@ -3,19 +3,19 @@
   <article class="formcontent">
     <h1>Register now</h1>
 
-    <div>
+    <div class="formStyle">
       <label class="formlabel" for="nickname">Nickname</label>
       <input class="input" required name="nickname" type="text" placeholder="Emile">
       <span class="error"><?php if (!empty($errors['nickname'])) echo $errors['nickname']; ?></span>
     </div>
 
-    <div>
+    <div class="formStyle">
       <label class="formlabel" for="email">E-mail</label>
       <input class="input" required name="email" type="email" placeholder="John.doe@gmail.com">
       <span class="error"><?php if (!empty($errors['email'])) echo $errors['email']; ?></span>
     </div>
 
-    <div>
+    <div class="formStyle">
       <label class="formlabel" for="password">Password</label>
       <input class="input" required name="password" type="text" placeholder="***********">
       <span class="error"><?php if (!empty($errors['password'])) echo $errors['password']; ?></span>
