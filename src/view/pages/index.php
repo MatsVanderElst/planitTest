884369a70f73d35538bbdc07af8410a2b18b30aa
emile catteeuw <emioe.catteeuw3@gmail.com>
2021-11-22 14:19:38 +0100

    deleted some demo code

--- src/view/pages/index.php
+++ src/view/pages/index.php
@@ -1,10 +1,3 @@
 <section class="content">
-  <ul class="demo-list">
-    <?php
-    foreach ($demos as $demo) {
-      echo ('<li>' . $demo->name . '</li>');
-      echo "glkjrshgkfjd";
-    }
-    ?>
-  </ul>
+ 
 </section>
