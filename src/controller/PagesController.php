356b3eadca92baa99287383dd47e4cafa224db33
emile catteeuw <emioe.catteeuw3@gmail.com>
2021-11-22 16:34:34 +0100

    added public function for register which adds users to db and checks for errors

--- src/controller/PagesController.php
+++ src/controller/PagesController.php
@@ -4,17 +4,60 @@ require_once __DIR__ . '/Controller.php';
 require_once __DIR__ . '/../model/User.php';
 
 
-class PagesController extends Controller {
+class PagesController extends Controller
+{
 
-  public function index() {
+  public function index()
+  {
     // this should refer to a database query, a hard-coded object is used for demo purposes
-   // $demos = Demo::all();
+    // $demos = Demo::all();
 
     //$demos = array(new Demo('first item'), new Demo('second item'), new Demo('last item'));
-   // $this->set('demos',$demos);
+    // $this->set('demos',$demos);
 
   }
-  public function register() {
-    
+  public function register()
+  {
+
+    //uitloggen wanneer je hier terechtkomt
+    unset($_SESSION['user']);
+
+    if (!empty($_POST['action'])) {
+      if (!empty($_POST['email'])) {
+        //kijken of email al bestaat
+        $exits = User::where('email', '=', $_POST['email'])->get();
+        if (!empty($exits[0])) {
+          $error['exists'] = "This e-mail is already in use.";
+          header('Location: index.php?page=register');
+
+          //exit wanneer email al bestaat
+          exit();
+        }
+
+      }
+      //is value van acrtion gelijk aan register?
+      if ($_POST["action"] === "register") {
+        //maakt nieuwe var aan voor user
+        $user = new User([
+          'nickname' => $_POST['nickname'],
+          'email' => $_POST['email'],
+          'password' => $_POST['password'],
+        ]);
+        //is input valid?
+        $errors = User::validate($user);
+        //geen errors? --> juiste user in session steken
+        if (empty($errors)) {
+          $_SESSION['user']['nickname'] = $user['nickname'];
+          
+          //in db steken
+          $user->save();
+          header('Location: index.php?page=personal');
+          exit();
+          //errors tonen als er zijn
+        } else {
+          $this->set('errors', $errors);
+        }
+      }
+    }
   }
 }
