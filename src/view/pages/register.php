e350575a67955a0f97803fed783d2c87c537c88f
emile catteeuw <emioe.catteeuw3@gmail.com>
2021-11-22 15:19:53 +0100

    form input and labels aangemaakt #3

--- src/view/pages/register.php
+++ src/view/pages/register.php
@@ -1,4 +1,21 @@
 <form class="registerform" action="index.php?page=register" method="POST">
   <input type="hidden" name="action" value="register" />
-  
+  <article class="formcontent">
+    <h1>Register now</h1>
+
+    <div>
+      <label class="formlabel" for="nickname">Nickname</label>
+      <input required name="nickname" type="text" placeholder="Emile">
+    </div>
+
+    <div>
+      <label class="formlabel" for="email">E-mail</label>
+      <input required name="email" type="email" placeholder="John.doe@gmail.com" >
+    </div>
+
+    <div>
+      <label class="formlabel" for="password">Password</label>
+      <input required name="password" type="text" placeholder="***********">
+    </div>
+  </article>
 </form>
