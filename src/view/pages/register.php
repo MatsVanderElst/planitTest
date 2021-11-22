80d6fd4999e12deb2fa36ce5cf32e1493d6fa96d
emile catteeuw <emioe.catteeuw3@gmail.com>
2021-11-22 17:30:41 +0100

    added class=input for validation purposes #3

--- src/view/pages/register.php
+++ src/view/pages/register.php
@@ -5,19 +5,19 @@
 
     <div>
       <label class="formlabel" for="nickname">Nickname</label>
-      <input required name="nickname" type="text" placeholder="Emile">
+      <input class="input" required name="nickname" type="text" placeholder="Emile">
       <span class="error"><?php if (!empty($errors['nickname'])) echo $errors['nickname']; ?></span>
     </div>
 
     <div>
       <label class="formlabel" for="email">E-mail</label>
-      <input required name="email" type="email" placeholder="John.doe@gmail.com">
+      <input class="input" required name="email" type="email" placeholder="John.doe@gmail.com">
       <span class="error"><?php if (!empty($errors['email'])) echo $errors['email']; ?></span>
     </div>
 
     <div>
       <label class="formlabel" for="password">Password</label>
-      <input required name="password" type="text" placeholder="***********">
+      <input class="input" required name="password" type="text" placeholder="***********">
       <span class="error"><?php if (!empty($errors['password'])) echo $errors['password']; ?></span>
     </div>
 
