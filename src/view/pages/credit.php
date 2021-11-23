8fa4f5f16c7b7f647cea6d1d520017edf1afd6b7
emile catteeuw <emioe.catteeuw3@gmail.com>
2021-11-23 13:02:05 +0100

    added bottom text to file (beware....) #6

--- src/view/pages/credit.php
+++ src/view/pages/credit.php
@@ -1,11 +1,17 @@
 <form class="creditform" action="index.php?page=credit" method="post">
   <input type="hidden" name="action" value="credit">
   <article class="formcontent">
-    <h1 class="formtitle">what is your weekly food budget?</h1>
+    <h1 class="formtitle">what is your weekly budget for food?</h1>
 
     <div class="formStyle">
       <label class="formlabel" for="credit">Budget</label>
       <input class="input" required name="credit" type="number" placeholder="100">
+    </div>
+
+    <div>
+      <p>Beware this budget is for groceries only. <br>
+        If you want to have a fun night out you should <br>
+        keep an amount on the side for drinking beers.</p>
     </div>
 
 
