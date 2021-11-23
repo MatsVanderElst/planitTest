3dd4cc4437ca7256bed5043c68966ae2e5568020
emile catteeuw <emioe.catteeuw3@gmail.com>
2021-11-23 12:59:28 +0100

    added html for budget form #6

---
+++ src/view/pages/credit.php
@@ -0,0 +1,14 @@
+<form class="creditform" action="index.php?page=credit" method="post">
+  <input type="hidden" name="action" value="credit">
+  <article class="formcontent">
+    <h1 class="formtitle">what is your weekly food budget?</h1>
+
+    <div class="formStyle">
+      <label class="formlabel" for="credit">Budget</label>
+      <input class="input" required name="credit" type="number" placeholder="100">
+    </div>
+
+
+    <input type="submit" value="submit" class="submitButton">
+  </article>
+</form>
