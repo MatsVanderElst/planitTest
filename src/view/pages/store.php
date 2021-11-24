84bf9c316a7fdbd46b707482050e7b2c20bc53bd
mats Van der Elst <matsvanderelst@matss-MacBook-Air.local>
2021-11-24 13:03:01 +0100

    added assets and started styling #3 #5

--- src/view/pages/store.php
+++ src/view/pages/store.php
@@ -1,15 +1,19 @@
 <form class="storeform" action="index.php?page=store" method="post">
   <input type="hidden" name="action" value="store">
   <article class="formcontent">
-    <h1 class="formtitle">store selection</h1>
+    <h1 class="formtitle">
+      <img src="../../assets/images/title.svg" alt="mealplanner" class="titleImage">
+    </h1>
 
     <div class="formStyle">
-      <input type="radio" id="carrefour" name="favstore" value="carrefour">
+      <input type="radio" class="store1" name="favstore" value="carrefour">
       <label for="carrefour">Carrefour</label><br>
-      <input type="radio" id="delhaize" name="favstore" value="delhaize">
+      <input type="radio" class="store2" name="favstore" value="delhaize">
       <label for="delhaize">Delhaize</label><br>
+      <input type="radio" class="store1" name="favstore" value="Albert Hein" select="selected">
+      <label for="Albert Hein">Albert Hein</label><br>
       <input type="radio" id="Albert Hein" name="favstore" value="Albert Hein" select="selected">
-      <label for="Albert Hein">Albert Hein</label><br><br>
+      <label for="Albert Hein">Albert Hein</label><br>
     </div>
 
     <input type="submit" value="favorite store" class="submitButton">
