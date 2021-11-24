590145363f58d751b3da5f471d7f103da576bc43
emile catteeuw <emioe.catteeuw3@gmail.com>
2021-11-24 22:48:27 +0100

    titel verborgen

--- src/view/layout.php
+++ src/view/layout.php
@@ -9,7 +9,7 @@
 </head>
 <body>
   <div class="container">
-      <header><h1 class="pagetitle">meal-planner</h1></header>
+      <header><h1 class="pagetitle hide">meal-planner</h1></header>
       <?php echo $content;?>
   </div>
   <?php echo $js; ?>
