301480e829a57fed6ae88f770ee228d622d1cae7
emile catteeuw <emioe.catteeuw3@gmail.com>
2021-11-27 18:02:38 +0100

    did some further styling on credit page #6

--- src/view/pages/credit.php
+++ src/view/pages/credit.php
@@ -1,22 +1,24 @@
+<h1 class="logintitle">
+  <img src="./assets/images/title.svg" alt="mealplanner" class="titleImage">
+</h1>
 <form class="creditform jsForm" action="index.php?page=credit" method="post">
   <input type="hidden" name="action" value="credit">
-  <article class="formcontent">
-    <h1 class="formtitle">what is your weekly budget for food?</h1>
+  <article class="formcontent__credit">
+    <img src="./assets/images/hamburger.svg" alt="hamburger illustration" class="creditburger">
+
 
     <div class="formStyle">
-      <label class="formlabel" for="credit">Budget</label>
+      <label class="formlabel" for="credit">Weekly budget</label>
       <input class="input" required name="credit" type="number" min="1" placeholder="100">
       <span class="error"><?php if (!empty($errors['credit'])) echo $errors['credit']; ?></span>
     </div>
 
 
     <div>
-      <p>Beware this budget is for groceries only. <br>
+      <p class="warning"> <span class="bold">Beware!</span> this budget is for groceries only. <br>
         If you want to have a fun night out you should <br>
         keep an amount on the side for drinking beers.</p>
     </div>
-
-
-    <input type="submit" value="submit" class="submitButton">
   </article>
+  <input type="submit" value="submit" class="submitButton">
 </form>
