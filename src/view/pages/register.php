5a769807d85bb66c0441d373c85098463dea0960
emile catteeuw <emioe.catteeuw3@gmail.com>
2021-11-27 15:58:34 +0100

    cleaned up some html from register page #3

--- src/view/pages/register.php
+++ src/view/pages/register.php
@@ -1,28 +1,28 @@
-<div class="form__container">
-  <form class="registerform jsForm" action="index.php?page=register" method="POST">
-    <input type="hidden" name="action" value="register" />
-    <article class="formcontent">
-      <h1 class="formtitle">Register now</h1>
+<h1 class="logintitle">
+  <img src="./assets/images/title.svg" alt="mealplanner" class="titleImage">
+</h1>
+<form class="registerform jsForm" action="index.php?page=register" method="POST">
+  <input type="hidden" name="action" value="register" />
+  <article class="formcontent">
 
-      <div class="formStyle">
-        <label class="formlabel" for="nickname">Nickname</label>
-        <input class="input" required name="nickname" type="text" placeholder="Emile">
-        <span class="error"><?php if (!empty($errors['nickname'])) echo $errors['nickname']; ?></span>
-      </div>
+    <div class="formStyle">
+      <label class="formlabel" for="nickname">Nickname</label>
+      <input class="input" required name="nickname" type="text" placeholder="Emile">
+      <span class="error"><?php if (!empty($errors['nickname'])) echo $errors['nickname']; ?></span>
+    </div>
 
-      <div class="formStyle">
-        <label class="formlabel" for="email">E-mail</label>
-        <input class="input" required name="email" type="email" placeholder="John.doe@gmail.com">
-        <span class="error"><?php if (!empty($errors['email'])) echo $errors['email']; ?></span>
-      </div>
+    <div class="formStyle">
+      <label class="formlabel" for="email">E-mail</label>
+      <input class="input" required name="email" type="email" placeholder="John.doe@gmail.com">
+      <span class="error"><?php if (!empty($errors['email'])) echo $errors['email']; ?></span>
+    </div>
 
-      <div class="formStyle">
-        <label class="formlabel" for="password">Password</label>
-        <input class="input" required name="password" type="password" placeholder="***********">
-        <span class="error"><?php if (!empty($errors['password'])) echo $errors['password']; ?></span>
-      </div>
+    <div class="formStyle">
+      <label class="formlabel" for="password">Password</label>
+      <input class="input" required name="password" type="password" placeholder="***********">
+      <span class="error"><?php if (!empty($errors['password'])) echo $errors['password']; ?></span>
+    </div>
 
-      <input type="submit" value="Register" class="submitButton">
-    </article>
-  </form>
-</div>
+    <input type="submit" value="Register" class="submitButton">
+  </article>
+</form>
