74f86baac95f3a66dc6387986abeed8bcbef091f
mats Van der Elst <matsvanderelst@matss-MacBook-Air.local>
2021-11-28 10:37:13 +0100

    Reworked html to make navigation buttons possible, started css. #7

--- src/view/pages/menu.php
+++ src/view/pages/menu.php
@@ -1,29 +1,37 @@
 <div class="menu__container">
-    <h1 class="logintitle">
+    <h1 class="menu__title">
         <img src="./assets/images/title.svg" alt="mealplanner" class="titleImage">
     </h1>
-    <h2> Student, you have 000 euro left  </h2>
+    <h2 class="menu__subtitle"> Student, you have 000 euro left  </h2>
     <p>Create or view your shopping lists or check out what food you still have at home!</p>
     <nav class="navigation__container">
         <div class="main__buttons">
-            <a class="list__button" href="#">
+            <div class="list__button">
+            <a href="#">
                 <p>Shoppig List</p>
                 <img src="#" alt="shopping list icon">
             </a>
-            <a class="products__button" href="#">
+            </div>
+            <div class="products__button">
+            <a href="#">
                 <p>products at home</p>
                 <img src="#" alt="home icon">
             </a>
+            </div>
         </div>
         <div class="secondary__buttons">
             <p>Discouts</p>
-            <a class="Discounts__button" href="#" >
-                <img src="#" alt="home icon">
-            </a>
+            <div class="Discounts__button">
+                <a  href="#" >
+                    <img src="#" alt="home icon">
+                </a>
+            </div>
             <p>Recipess</p>
-            <a class="recipes__button" href="#">
-                <img src="#" alt="home icon">
-            </a>
+            <div class="recipes__button">
+                <a href="#">
+                    <img src="#" alt="home icon">
+                </a>
+            </div>
         </div>
     </nav>
     <footer>
