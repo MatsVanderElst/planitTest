001f6d7b7380954c64ffdd96d257fa55e111a533
mats Van der Elst <matsvanderelst@matss-MacBook-Air.local>
2021-11-28 18:46:21 +0100

    added assets and cotiuned writing css #7

--- src/view/pages/menu.php
+++ src/view/pages/menu.php
@@ -3,19 +3,19 @@
         <img src="./assets/images/title.svg" alt="mealplanner" class="titleImage">
     </h1>
     <h2 class="menu__subtitle"> Student, you have 000 euro left  </h2>
-    <p>Create or view your shopping lists or check out what food you still have at home!</p>
+    <p class="menu__paragraph">Create or view your shopping lists or check out what food you still have at home!</p>
     <nav class="navigation__container">
         <div class="main__buttons">
             <div class="list__button">
             <a href="#">
                 <p>Shoppig List</p>
-                <img src="#" alt="shopping list icon">
+                <img src="assets/images/listIcon.svg" alt="shopping list icon">
             </a>
             </div>
             <div class="products__button">
             <a href="#">
                 <p>products at home</p>
-                <img src="#" alt="home icon">
+                <img src="assets/images/houseIcon.svg" alt="home icon">
             </a>
             </div>
         </div>
@@ -23,20 +23,20 @@
             <p>Discouts</p>
             <div class="Discounts__button">
                 <a  href="#" >
-                    <img src="#" alt="home icon">
+                    <img src="assets/images/ticketIcon.svg" alt="ticket icon">
                 </a>
             </div>
             <p>Recipess</p>
             <div class="recipes__button">
                 <a href="#">
-                    <img src="#" alt="home icon">
+                    <img src="assets/images/hamburgerIcon.svf" alt="hamburger icon">
                 </a>
             </div>
         </div>
     </nav>
     <footer>
         <a>
-        <img src="#" alt="settings icon">
+        <img src="assets/images/settingsIcon.svg" alt="settings icon">
         </a>
     </footer>
 </div>
