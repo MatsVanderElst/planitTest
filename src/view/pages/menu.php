8777751864ccaf24614b17fb9b3e6f943c5ef4d5
emile catteeuw <emioe.catteeuw3@gmail.com>
2021-11-30 14:00:52 +0100

    added functionality that can show user how much credit he has (xxx credit left) #7

--- src/view/pages/menu.php
+++ src/view/pages/menu.php
@@ -1,42 +1,42 @@
 <div class="menu__container">
-    <h1 class="menu__title">
-        <img src="./assets/images/title.svg" alt="mealplanner" class="titleImage">
-    </h1>
-    <h2 class="menu__subtitle"> Student, you have 000 $ left  </h2>
-    <p class="menu__paragraph">Create or view your shopping lists or check out what food you still have at home!</p>
-    <nav class="navigation__container">
-        <div class="main__buttons">
-            <div class="list__button">
-            <a href="#">
-                <p>Shoppig List</p>
-                <img src="assets/images/listIcon.svg" alt="shopping list icon">
-            </a>
-            </div>
-            <div class="products__button">
-            <a href="#">
-                <p>products at home</p>
-                <img src="assets/images/houseIcon.svg" alt="home icon">
-            </a>
-            </div>
-        </div>
-        <div class="secondary__buttons">
-            <p class="discounts__title">Discouts</p>
-            <div class="discounts__button">
-                <a  href="#" >
-                    <img src="assets/images/ticketIcon.svg" alt="ticket icon">
-                </a>
-            </div>
-            <p class="recipes__title">Recipess</p>
-            <div class="recipes__button">
-                <a href="#">
-                    <img src="assets/images/hamburgerIcon.svg" alt="hamburger icon">
-                </a>
-            </div>
-        </div>
-    </nav>
-    <footer>
-        <a>
-            <img class="settings__button" src="assets/images/settingsIcon.svg" alt="settings icon">
+  <h1 class="menu__title">
+    <img src="./assets/images/title.svg" alt="mealplanner" class="titleImage">
+  </h1>
+  <h2 class="menu__subtitle"> Student, you have € <?php echo $_SESSION['user']['credit'] ?> left </h2>
+  <p class="menu__paragraph">Create or view your shopping lists or check out what food you still have at home!</p>
+  <nav class="navigation__container">
+    <div class="main__buttons">
+      <div class="list__button">
+        <a href="#">
+          <p>Shoppig List</p>
+          <img src="assets/images/listIcon.svg" alt="shopping list icon">
         </a>
-    </footer>
+      </div>
+      <div class="products__button">
+        <a href="#">
+          <p>products at home</p>
+          <img src="assets/images/houseIcon.svg" alt="home icon">
+        </a>
+      </div>
+    </div>
+    <div class="secondary__buttons">
+      <p class="discounts__title">Discouts</p>
+      <div class="discounts__button">
+        <a href="#">
+          <img src="assets/images/ticketIcon.svg" alt="ticket icon">
+        </a>
+      </div>
+      <p class="recipes__title">Recipess</p>
+      <div class="recipes__button">
+        <a href="#">
+          <img src="assets/images/hamburgerIcon.svg" alt="hamburger icon">
+        </a>
+      </div>
+    </div>
+  </nav>
+  <footer>
+    <a>
+      <img class="settings__button" src="assets/images/settingsIcon.svg" alt="settings icon">
+    </a>
+  </footer>
 </div>
