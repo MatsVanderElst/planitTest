9bd8552dc0d73449a31756edf9c3151a5a11c2e7
emile catteeuw <emioe.catteeuw3@gmail.com>
2021-12-01 16:00:09 +0100

    added require once on top of file in order to link model w pagesctrlr DUH #8

--- src/controller/PagesController.php
+++ src/controller/PagesController.php
@@ -2,6 +2,8 @@
 
 require_once __DIR__ . '/Controller.php';
 require_once __DIR__ . '/../model/User.php';
+require_once __DIR__ . '/../model/Product.php';
+
 
 
 class PagesController extends Controller {
