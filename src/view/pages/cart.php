f82836d76e13231b0f5040c8693f433783993927
emile catteeuw <emioe.catteeuw3@gmail.com>
2021-12-02 12:26:40 +0100

    added html to display products from list, delete icon, total price and confirm button #9

--- src/view/pages/cart.php
+++ src/view/pages/cart.php
@@ -1,14 +1,50 @@
+<div>
+  <?php if (empty($_SESSION['list'])) : ?>
+    <div>
+      <p> Your shoppinglist is still empty</p>
+      <a href="index.php?page=list">make A shoppinglist</a>
+    </div>
+  <?php endif; ?>
+
   <?php
   $total = 0;
   foreach ($selectedProducts as $product)
     $total += $product[0]['price'];
   ?>
 
+
   <?php if (!empty($_SESSION['list'])) : ?>
-    <div class="totalFlex marginTop">
-      <section class="totalFlex">
-        <p class="space totalStyle">Total</p>
-        <p class="space evenMoreSpace"><?php echo $total ?></p>
+    <?php foreach ($selectedProducts as $product) : ?>
+      <div>
+        <div>
+          <div>
+            <span>
+              <a href="index.php?page=cart&action=delete&product_product=<?php echo array_search($product, $selectedProducts) ?>">
+                <span class="material-icons bin">
+                  delete
+                </span>
+              </a>
+            </span>
+          </div>
+          <p><?php echo $product[0]['product'] ?></p>
+          <p><?php echo $product[0]['price'] ?></p>
+        </div>
+      </div>
+      <hr class="stroke">
+    <?php endforeach; ?>
+  <?php endif; ?>
+
+
+  <?php if (!empty($_SESSION['list'])) : ?>
+    <div>
+
+      <section>
+        <p>Total</p>
+        <p><?php echo $total ?></p>
       </section>
+
+      <a href="index.php?page=menu">confirm</a>
     </div>
   <?php endif; ?>
+
+</div>
