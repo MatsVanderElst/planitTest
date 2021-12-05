0dac329c25ea3e7acec3e561423442eebe9a8995
emile catteeuw <emioe.catteeuw3@gmail.com>
2021-12-05 15:51:59 +0100

    added some more css to cart page #9

--- src/view/pages/cart.php
+++ src/view/pages/cart.php
@@ -9,53 +9,53 @@
   <?php
   $total = 0;
   foreach ($selectedProducts as $product)
-  if ($_SESSION['user']['favstore'] == 'delhaize') {
-    $total += $product[0]['price'] + 0.4;
-  } elseif ($_SESSION['user']['favstore'] == 'carrefour') {
-    $total += $product[0]['price'] - 0.2;
-  } elseif ($_SESSION['user']['favstore'] == 'colruyt') {
-    $total += $product[0]['price'] - 0.5;
-  } elseif ($_SESSION['user']['favstore'] == 'alberthein') {
-    $total += $product[0]['price'] - 0.3;
-  } else {
-    $total += $product[0]['price'];
-  }
+    if ($_SESSION['user']['favstore'] == 'delhaize') {
+      $total += $product[0]['price'] + 0.4;
+    } elseif ($_SESSION['user']['favstore'] == 'carrefour') {
+      $total += $product[0]['price'] - 0.2;
+    } elseif ($_SESSION['user']['favstore'] == 'colruyt') {
+      $total += $product[0]['price'] - 0.5;
+    } elseif ($_SESSION['user']['favstore'] == 'alberthein') {
+      $total += $product[0]['price'] - 0.3;
+    } else {
+      $total += $product[0]['price'];
+    }
   ?>
 
-  
 
 
-  <?php if (!empty($_SESSION['list'])) : ?>
-    <?php foreach ($selectedProducts as $product) : ?>
-      <div>
+  <div class="cart__products">
+    <?php if (!empty($_SESSION['list'])) : ?>
+      <?php foreach ($selectedProducts as $product) : ?>
         <div>
-          <div>
-            <span>
-              <a href="index.php?page=cart&action=delete&product_product=<?php echo array_search($product, $selectedProducts) ?>">
-                <span class="material-icons bin">
-                  delete
-                </span>
-              </a>
-            </span>
+          <div class="single__product">
+            <div>
+              <span>
+                <a href="index.php?page=cart&action=delete&product_product=<?php echo array_search($product, $selectedProducts) ?>">
+                  <span class="material-icons bin">
+                    delete
+                  </span>
+                </a>
+              </span>
+            </div>
+            <p><?php echo $product[0]['product'] ?></p>
+            <?php if ($_SESSION['user']['favstore'] == 'delhaize') : ?>
+              <p class="product__price"><?php echo $product[0]['price'] + 0.4 ?></p>
+            <?php elseif ($_SESSION['user']['favstore'] == 'carrefour') : ?>
+              <p class="product__price"><?php echo $product[0]['price'] - 0.2 ?></p>
+            <?php elseif ($_SESSION['user']['favstore'] == 'colruyt') : ?>
+              <p class="product__price"><?php echo $product[0]['price'] - 0.5 ?></p>
+            <?php elseif ($_SESSION['user']['favstore'] == 'alberthein') : ?>
+              <p class="product__price"><?php echo $product[0]['price'] - 0.3 ?></p>
+            <?php else : ?>
+              <p class="product__price"><?php echo $product[0]['price'] ?></p>
+            <?php endif; ?>
           </div>
-          <p><?php echo $product[0]['product'] ?></p>
-          <?php if ($_SESSION['user']['favstore'] == 'delhaize') : ?>
-            <p class="product__price"><?php echo $product[0]['price'] + 0.4 ?></p>
-          <?php elseif ($_SESSION['user']['favstore'] == 'carrefour') : ?>
-            <p class="product__price"><?php echo $product[0]['price'] - 0.2 ?></p>
-          <?php elseif ($_SESSION['user']['favstore'] == 'colruyt') : ?>
-            <p class="product__price"><?php echo $product[0]['price'] - 0.5 ?></p>
-          <?php elseif ($_SESSION['user']['favstore'] == 'alberthein') : ?>
-            <p class="product__price"><?php echo $product[0]['price'] - 0.3 ?></p>
-          <?php else : ?>
-            <p class="product__price"><?php echo $product[0]['price'] ?></p>
-          <?php endif; ?>
         </div>
-      </div>
-      <hr class="stroke">
-    <?php endforeach; ?>
-  <?php endif; ?>
-
+        <hr class="stroke">
+      <?php endforeach; ?>
+    <?php endif; ?>
+  </div>
 
   <?php if (!empty($_SESSION['list'])) : ?>
     <div>
