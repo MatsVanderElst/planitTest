063dfef0e8430232ce8058b91d766c2e59524b46
emile catteeuw <emioe.catteeuw3@gmail.com>
2021-12-11 18:50:02 +0100

    changed some html in form and changed some classnames, also added css tyling to settings page #17

--- src/view/pages/settings.php
+++ src/view/pages/settings.php
@@ -6,46 +6,48 @@
     <form class="creditformTwo jsForm" action="index.php?page=settings" method="post">
       <input type="hidden" name="action" value="credit">
       <article class="formcontent__credit">
-        <div class="formStyle">
+        <div class="formStyleTwo">
           <label class="formlabel" for="credit">Add money to your remaining budget</label>
           <input class="input" required name="credit" type="number" min="1" placeholder="100">
+          <input type="submit" value="submit" class="submitButton extraMarginTop">
           <span class="error"><?php if (!empty($errors['credit'])) echo $errors['credit']; ?></span>
         </div>
       </article>
-      <input type="submit" value="submit" class="submitButton">
+
     </form>
 
     <form class="creditformTwo" action="index.php?page=settings" method="post">
       <input type="hidden" name="action" value="store">
       <article class="formcontent__credit">
         <div class="formStyleTwo">
-          <label class="formlabel" for="store">What's your favorite grocerystore?</label>
-          <table class="stores">
-            <tr>
-              <td><input class="storeRadioButtonTwo" name="store" type="radio" value="carrefour" required></td>
-              <td><label class="labelStyling" for="store">Carrefour</label></td>
-            </tr>
-            <tr>
-              <td><input class="storeRadioButtonTwo" name="store" type="radio" value="delhaize"></td>
-              <td><label class="labelStyling" for="store">Delhaize</label></td>
-            </tr>
-            <tr>
-              <td><input class="storeRadioButtonTwo" name="store" type="radio" value="colruyt"></td>
-              <td><label class="labelStyling" for="store">Colruyt</label></td>
-            </tr>
-            <tr>
-              <td><input class="storeRadioButtonTwo" name="store" type="radio" value="alberthein"></td>
-              <td><label class="labelStyling" for="store">Albert Hein</label></td>
-            </tr>
-            <tr>
-              <td><input class="storeRadioButtonTwo" name="store" type="radio" value="aldi"></td>
-              <td><label class="labelStyling" for="store">Aldi</label></td>
-            </tr>
-          </table>
-        </div><br>
-        <span class="error"><?php if (!empty($errors['store'])) echo $errors['store']; ?></span>
+          <label class="formlabel extraMargin" for="store">What's your favorite grocerystore?</label>
+          <div class="stores">
+            <article class="choices">
+              <input class="storeRadioButtonTwo" name="store" type="radio" value="carrefour" required>
+              <label class="labelStyling" for="store">Carrefour</label>
+            </article>
+            <article class="choices">
+              <input class="storeRadioButtonTwo" name="store" type="radio" value="delhaize">
+              <label class="labelStyling" for="store">Delhaize</label>
+            </article>
+            <article class="choices">
+              <input class="storeRadioButtonTwo" name="store" type="radio" value="colruyt">
+              <label class="labelStyling" for="store">Colruyt</label>
+            </article>
+            <article class="choices">
+              <input class="storeRadioButtonTwo" name="store" type="radio" value="alberthein">
+              <label class="labelStyling" for="store">Albert Hein</label>
+            </article>
+            <article class="choices">
+              <input class="storeRadioButtonTwo" name="store" type="radio" value="aldi">
+              <label class="labelStyling" for="store">Aldi</label>
+            </article>
+          </div>
+          <input type="submit" value="Submit" class="submitButton extraMarginTop">
+          <span class="error"><?php if (!empty($errors['store'])) echo $errors['store']; ?></span>
+        </div>
       </article>
-      <input type="submit" value="Submit" class="submitButton">
+
     </form>
   </div>
 </div>
