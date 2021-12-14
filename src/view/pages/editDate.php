ce199ad6dc4bb5e18945ff890d930ed1125cad98
mats Van der Elst <matsvanderelst@matss-MacBook-Air.local>
2021-12-14 16:57:20 +0100

    added further styling for editDate page #18

--- src/view/pages/editDate.php
+++ src/view/pages/editDate.php
@@ -2,12 +2,12 @@
 <h1 class="logintitle">
   <img src="./assets/images/title.svg" alt="mealplanner" class="titleImage">
 </h1>
+<div class="editDateHeader__container">
+  <p class="editDateParagraph">Here you can edit the expiration date of this <?php echo $fridgeItem->product['product'] ?></p>
+  <p>the current expiration date of this product is: <?php echo $fridgeItem['expiration_date'] ?></p>
+</div>
 <article class="formcontent__editDate">
    
-    <div class="editDateHeader__container">
-      <p class="feditDateParagraph">Here you can edit the expiration date of this <?php echo $fridgeItem->product['product'] ?></p>
-    </div>
-    <p>the current expiration date of this product is: <?php echo $fridgeItem['expiration_date'] ?></p>
 
     <form class="editDate_form" method="get" action="index.php?">
         <input name="action" type="hidden" value="editDate">
