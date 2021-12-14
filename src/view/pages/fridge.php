53443c54fb4a8207c050cbca5e6c1b6e8b57a801
emile catteeuw <emioe.catteeuw3@gmail.com>
2021-12-14 13:06:16 +0100

    added material icons instead of button #18

--- src/view/pages/fridge.php
+++ src/view/pages/fridge.php
@@ -1,7 +1,7 @@
 <div class="fridgeBackroundContainer">
-<h1 class="logintitle">
-  <img src="./assets/images/title.svg" alt="mealplanner" class="titleImage">
-</h1>
+  <h1 class="logintitle">
+    <img src="./assets/images/title.svg" alt="mealplanner" class="titleImage">
+  </h1>
   <article class="formcontent__fridge">
     <img src="./assets/images/hamburger.svg" alt="hamburger illustration" class="fridgeburger">
     <div class="fridgeHeader__container">
@@ -9,35 +9,35 @@
       <p class="fridgeParagraph">Here’s an overview of the products you Have at home. Tap the name to see it’s details.</p>
     </div>
     <ul class="productsList">
-        <?php foreach ($fridge as $fridgeItem) : ?>
-            <li class="fridgeItem">
-                <a>
-                  <a href="index.php?page=productDetail&detailedProduct=<?php echo $fridgeItem->product['id'] ?>">
-                    <p class="fridgeItem__name"><?php echo $fridgeItem->product['product']; ?></p>
-                  </a>
-                    <p class="fridgeItem__quantity"><?php echo $fridgeItem['quantity']; ?></p>
-                  <div>
-                    <p class="fridgeItem__expiration date"><?php echo $fridgeItem['expiration_date']; ?></p> 
-                  </a>
-                  <a href="index.php?page=editDate&fridgeItemId=<?php echo $fridgeItem['id']; ?>">
-                  <span class="material-icons bin">
-                    <button class="fridgeItem__detailButton">edit date</button>
-                  </span>
-                  </a>
-                  </div>
-                  <span>
-                  <a href="index.php?page=fridge&action=use&productId=<?php echo $fridgeItem['id'] ?>">
-                    <span class="material-icons bin">
-                      delete
-                    </span>
-                  </a>
-                  </span>
-            </li>
-        <?php endforeach; ?>
-    </ul>    
-    <a href="index.php?page=menu">
-          <button class="backButton">Back</button>
-    </a>
-  </article>
-  <!-- <input type="submit" value="fridgedelete" class="submitButton"> -->
+      <?php foreach ($fridge as $fridgeItem) : ?>
+        <li class="fridgeItem">
+          <a>
+            <a href="index.php?page=productDetail&detailedProduct=<?php echo $fridgeItem->product['id'] ?>">
+              <p class="fridgeItem__name"><?php echo $fridgeItem->product['product']; ?></p>
+            </a>
+            <p class="fridgeItem__quantity"><?php echo $fridgeItem['quantity']; ?></p>
+            <div>
+              <p class="fridgeItem__expiration date"><?php echo $fridgeItem['expiration_date']; ?></p>
+          </a>
+          <a href="index.php?page=editDate&fridgeItemId=<?php echo $fridgeItem['id']; ?>">
+            <span class="material-icons bin">
+              edit
+            </span>
+          </a>
+</div>
+<span>
+  <a href="index.php?page=fridge&action=use&productId=<?php echo $fridgeItem['id'] ?>">
+    <span class="material-icons bin">
+      delete
+    </span>
+  </a>
+</span>
+</li>
+<?php endforeach; ?>
+</ul>
+<a href="index.php?page=menu">
+  <button class="backButton">Back</button>
+</a>
+</article>
+<!-- <input type="submit" value="fridgedelete" class="submitButton"> -->
 </div>
