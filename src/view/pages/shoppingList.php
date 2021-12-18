85d8d32e4c9c62ce5eacede5643c6846dbc389cf
emile catteeuw <emioe.catteeuw3@gmail.com>
2021-12-18 19:38:08 +0100

    started styling for shoplist page #10

--- src/view/pages/shoppingList.php
+++ src/view/pages/shoppingList.php
@@ -4,28 +4,38 @@
       <img src="./assets/images/title.svg" alt="mealplanner" class="titleImage-shoppingList">
     </a>
   </h1>
-  <div>
-  <?php if (empty($shoppingLists)) : ?>  
-    <div class="empty">
+  <div class="shopListContainer">
+    <?php if (empty($shoppingLists)) : ?>
+      <div class="empty">
         <p class="emptyP"> Your shopping list is still empty</p>
         <a class="emptyA" href="index.php?page=list">click to create a shopping list</a>
-        <?php endif; ?>
+      <?php endif; ?>
+      <div>
         <?php if (!empty($shoppingLists)) : ?>
           <p class="emptyP"> Your saved shopping lists are</p>
-          <?php foreach($shoppingLists as $shoppingList):?>
-            <div class="list_listItem">
-                <a class="emptyA" href="index.php?page=listDetail&listId=<?php echo $shoppingList['id']; ?>"><p><?php echo $shoppingList['name']; ?></p></a>
-                <span>
-                <a href="index.php?page=shoppingList&action=delete&listId=<?php echo $shoppingList['id'] ?>">
-                  <span class="material-icons bin">
-                    delete
-                  </span>
-                </a>
-              </span>
-            </div>
-          <?php endforeach; ?>
-        <?php endif; ?>
-      </div>    
+      </div>
+      <div class="item__container">
+        <?php foreach ($shoppingLists as $shoppingList) : ?>
+          <div class="list_listItem">
+            <section>
+              <a class="shopListLink" href="index.php?page=listDetail&listId=<?php echo $shoppingList['id']; ?>">
+                <p><?php echo $shoppingList['name']; ?></p>
+                <img src="assets/images/listIcon.svg" alt="shopping list icon" width="60">
+              </a>
+
+            </section>
+            <span>
+              <a href="index.php?page=shoppingList&action=delete&listId=<?php echo $shoppingList['id'] ?>">
+                <span class="material-icons bin">
+                  delete
+                </span>
+              </a>
+            </span>
+          </div>
+        <?php endforeach; ?>
+      </div>
+    <?php endif; ?>
+      </div>
 
   </div>
 </div>
