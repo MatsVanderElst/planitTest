f6369f862ad0b94163b09b099fb2ad133c14aef9
emile catteeuw <emioe.catteeuw3@gmail.com>
2021-12-19 21:48:36 +0100

    finished styling for editDate page #18

--- src/view/pages/editDate.php
+++ src/view/pages/editDate.php
@@ -1,21 +1,21 @@
 <div class="editDateContainer">
-<h1 class="logintitle">
-  <img src="./assets/images/title.svg" alt="mealplanner" class="titleImage editDate">
-</h1>
-<div class="editDateHeader__container">
-  <p class="editDateParagraph">Here you can edit the expiration date of this <?php echo $fridgeItem->product['product'] ?></p>
-  <p>the current expiration date of this product is: <?php echo $fridgeItem['expiration_date'] ?></p>
-</div>
-<article class="formcontent__editDate">
+  <h1 class="logintitle">
+    <img src="./assets/images/title.svg" alt="mealplanner" class="titleImage editDate">
+  </h1>
+  <div class="editDateHeader__container">
+    <p>Edit the expiration date of this <?php echo $fridgeItem->product['product'] ?>.</p>
+    <p class="break">The current expiration date is displayed below</p>
+  </div>
+  <article class="formcontent__editDate">
     <form class="editDate_form" method="get" action="index.php?">
-        <input name="action" type="hidden" value="editDate">
-        <input name="page" type="hidden" value="editDate">
-        <input name="fridgeItemId" type="hidden" value="<?php echo $fridgeItem['id'] ?>">
-        <input name="newDate" type="date" value="<?php echo $fridgeItem['expiration_date'] ?>">
-        <input type="submit" value="Save" class="submitButton">
+      <input name="action" type="hidden" value="editDate">
+      <input name="page" type="hidden" value="editDate">
+      <input name="fridgeItemId" type="hidden" value="<?php echo $fridgeItem['id'] ?>">
+      <input class="newDate" name="newDate" type="date" value="<?php echo $fridgeItem['expiration_date'] ?>">
+      <input type="submit" value="Save" class="submitButton">
     </form>
-    <a href="index.php?page=fridge">
-      <button class="backButton">Back</button>
-    </a>
-</article>
+  </article>
+  <a href="index.php?page=fridge">
+    <button class="backButton">Back</button>
+  </a>
 </div>
