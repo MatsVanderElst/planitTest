906ac93e425389eec16b52e19e3913ab19b42207
mats Van der Elst <matsvanderelst@matss-MacBook-Air.local>
2021-12-21 00:59:16 +0100

    added styling to listDetail without overlapping classnames #10

--- src/view/pages/listDetail.php
+++ src/view/pages/listDetail.php
@@ -1,27 +1,27 @@
-<h1 class="logintitle">
+<h1 class="loginDetailtitle">
   <img src="./assets/images/title.svg" alt="mealplanner" class="productDettailTitleImage">
 </h1>
 <article class="formcontent__listDetail">
     <div class="listDetailHeader__container">
     </div>
-    <article class="welcome-list">
+    <article class="welcome-listDetail">
     <section>
-      <p class="list__welcome"><?php echo $_SESSION['user']['nickname'] ?>, <br> you have € <?php echo $_SESSION['overschot'] ?> left this week</p>
+      <p class="listDetail__welcome"><?php echo $_SESSION['user']['nickname'] ?>, <br> you have € <?php echo $_SESSION['overschot'] ?> left this week</p>
     </section>
     <section>
-      <p class="list__text">Take a look at the products on your list. <br> <br>
+      <p class="listDetail__text">Take a look at the products on your list. <br> <br>
         click on the cart to add it to your cart, the items you choose will be subtracted From your budget. </p>
     </section>
     </article>
-    <div class="product__list-container">
+    <div class="product__listDetail-container">
         <a href="index.php?page=list&listId=<?php echo $listId; ?>">
-        <span class="material-icons cart">
+        <span class="material-icons cart listDetailIcon">
             shopping_cart
         </span>
         </a>
-        <article class="product__list">
+        <article class="product__listDetail">
         <?php foreach ($products as $product) : ?>
-            <div class="product__single">
+            <div class="product__singleDetail">
             <p class="product__name"><?php echo $product['product'] ?></p>
             <?php if ($product['discountStorePrice'] != 0) : ?>
                 <p class="product__price-red"><?php echo number_format((float)$product['discountStorePrice'], 2, '.', ''); ?></p>
@@ -36,6 +36,6 @@
   
 
     <a href="index.php?page=shoppingList">
-          <button class="backButton">Back</button>
+          <button class="listDetailBackButton">Back</button>
     </a>
   </article>
